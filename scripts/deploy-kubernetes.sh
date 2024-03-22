8baaf142b8dc2dc2b42cb781e8fedf8d1f30aa4e
Tobias Peslalz <peslalz.tobias@gmail.com>
2024-03-22 19:54:47 +0100

    :sparkles: add loadbalancer servicetype and kompose fix for it

--- scripts/deploy-kubernetes.sh
+++ scripts/deploy-kubernetes.sh
@@ -4,6 +4,9 @@ kubectl delete -f ./kubernetes/
 rm -f ./kubernetes/*.yaml
 
 kompose convert -f ./docker/docker-compose.yml -o ./kubernetes/
+# remove -tcp suffix from loadbalanced services
+find kubernetes/ -type f -name "*-tcp-service.yaml" -exec sed -i 's/\(name: \)\(.*\)-tcp/\1\2/g' {} \;
+
 kubectl config view
 
 kubectl apply -f ./kubernetes/
