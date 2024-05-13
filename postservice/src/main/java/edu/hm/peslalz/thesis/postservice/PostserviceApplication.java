ab938e38042ae31698cd0bdd39cae0e06a26251c
Tobias Peslalz <peslalz.tobias@gmail.com>
2024-05-13 19:49:23 +0200

    :tada: (postservice) init the postservice

---
+++ postservice/src/main/java/edu/hm/peslalz/thesis/postservice/PostserviceApplication.java
@@ -0,0 +1,13 @@
+package edu.hm.peslalz.thesis.postservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostserviceApplication.class, args);
+    }
+
+}
