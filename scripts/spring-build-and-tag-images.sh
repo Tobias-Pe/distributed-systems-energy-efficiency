93fb355d77bfaf76588ed5ca00675d9a6e078231
Tobias Peslalz <peslalz.tobias@gmail.com>
2024-05-13 19:51:25 +0200

    :bug: (script) remove windows line endings

--- scripts/spring-build-and-tag-images.sh
+++ scripts/spring-build-and-tag-images.sh
@@ -3,7 +3,7 @@
 mvn spring-boot:build-image -DskipTests
 VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
-image_names=($(cat pom.xml | grep "<module>" | sed 's/\s*<.*>\(.*\)<.*>/\1/'))
+image_names=($(cat pom.xml | grep "<module>" | sed 's/\s*<.*>\(.*\)<.*>/\1/' | tr -d '\r'))
 
 for image_name in "${image_names[@]}"; do
     docker tag ${image_name}:${VERSION} ghcr.io/tobias-pe/microservices_energyefficiency/${image_name}:latest
