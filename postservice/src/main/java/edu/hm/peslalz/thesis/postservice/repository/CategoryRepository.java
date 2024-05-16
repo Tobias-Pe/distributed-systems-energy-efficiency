460bc2af7f67d7bd8478863933ca4fddc2cc7607
Tobias Peslalz <peslalz.tobias@gmail.com>
2024-05-16 19:52:07 +0200

    :adhesive_bandage: (postservice) small fixes

--- postservice/src/main/java/edu/hm/peslalz/thesis/postservice/repository/CategoryRepository.java
+++ postservice/src/main/java/edu/hm/peslalz/thesis/postservice/repository/CategoryRepository.java
@@ -1,8 +1,7 @@
 package edu.hm.peslalz.thesis.postservice.repository;
 
 import edu.hm.peslalz.thesis.postservice.entity.Category;
-import edu.hm.peslalz.thesis.postservice.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CatergoryRepository extends JpaRepository<Category, String> {
+public interface CategoryRepository extends JpaRepository<Category, String> {
 }
