360597c6afb8ae8f083c395db4b2c001dde20fd5
Tobias Peslalz <peslalz.tobias@gmail.com>
2024-05-17 18:41:20 +0200

    :adhesive_bandage: add api description, make more restful & use pagination where possible

--- postservice/src/main/java/edu/hm/peslalz/thesis/postservice/service/CategoryService.java
+++ postservice/src/main/java/edu/hm/peslalz/thesis/postservice/service/CategoryService.java
@@ -2,9 +2,10 @@ package edu.hm.peslalz.thesis.postservice.service;
 
 import edu.hm.peslalz.thesis.postservice.entity.*;
 import edu.hm.peslalz.thesis.postservice.repository.CategoryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 public class CategoryService {
@@ -14,7 +15,7 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
-    public List<Category> getCategories() {
-        return categoryRepository.findAll();
+    public Page<Category> getCategories(@RequestParam(defaultValue = "0") int page) {
+        return categoryRepository.findAll(PageRequest.of(page, 50));
     }
 }
--- userservice/src/main/java/edu/hm/peslalz/thesis/userservice/controller/UserAccountController.java
+++ userservice/src/main/java/edu/hm/peslalz/thesis/userservice/controller/UserAccountController.java
@@ -3,6 +3,7 @@ package edu.hm.peslalz.thesis.userservice.controller;
 import edu.hm.peslalz.thesis.userservice.entity.UserAccount;
 import edu.hm.peslalz.thesis.userservice.entity.UserAccountRequest;
 import edu.hm.peslalz.thesis.userservice.service.UserAccountService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -20,33 +21,39 @@ public class UserAccountController {
         this.userAccountService = userAccountService;
     }
 
+    @Operation(description = "Create a user")
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public UserAccount createUserAccount(@RequestBody UserAccountRequest userAccountRequest) {
         return userAccountService.createUser(userAccountRequest);
     }
 
+    @Operation(description = "Search a user (% for wildcard)")
     @PostMapping("/search")
     public Page<UserAccount> searchUser(@RequestParam String query, @RequestParam(defaultValue = "0") int page) {
         return userAccountService.search(query, page);
     }
 
+    @Operation(description = "Get information to a user")
     @GetMapping("/{id}")
     public UserAccount getUserAccount(@PathVariable int id) {
         return userAccountService.getUserById(id);
     }
 
-    @PostMapping("/{id}")
+    @Operation(description = "Update a user")
+    @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
     public UserAccount updateUser(@RequestBody UserAccountRequest userAccountRequest, @PathVariable Integer id) {
         return userAccountService.updateUser(userAccountRequest, id);
     }
 
-    @PostMapping("/{id}/follow")
+    @Operation(description = "Follow a user")
+    @PutMapping("/{id}/follow")
     public UserAccount followUser(@RequestParam String toBeFollowedUsername, @PathVariable Integer id) {
         return userAccountService.follow(id, toBeFollowedUsername);
     }
 
+    @Operation(description = "Get followers of a user")
     @GetMapping("/{id}/followers")
     public Set<UserAccount> getFollowers(@PathVariable Integer id) {
         return userAccountService.getFollowers(id);
