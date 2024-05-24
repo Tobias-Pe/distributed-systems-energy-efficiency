61134290350085a3ac0504db2bf9c1c7baae1148
Tobias Peslalz <peslalz.tobias@hm.edu>
2024-05-24 13:57:21 +0200

    :white_check_mark: (notificationservice) add tests for all scenarios

--- notificationservice/src/main/java/edu/hm/peslalz/thesis/notificationservice/entity/UserMessage.java
+++ notificationservice/src/main/java/edu/hm/peslalz/thesis/notificationservice/entity/UserMessage.java
@@ -1,6 +1,8 @@
 package edu.hm.peslalz.thesis.notificationservice.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
@@ -8,6 +10,8 @@ import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserMessage {
     private Integer id;
     private String username;
--- notificationservice/src/test/java/edu/hm/peslalz/thesis/notificationservice/NotificationserviceApplicationTests.java
+++ notificationservice/src/test/java/edu/hm/peslalz/thesis/notificationservice/NotificationserviceApplicationTests.java
@@ -1,13 +1,93 @@
 package edu.hm.peslalz.thesis.notificationservice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import edu.hm.peslalz.thesis.notificationservice.client.UserClient;
+import edu.hm.peslalz.thesis.notificationservice.controller.NotificationController;
+import edu.hm.peslalz.thesis.notificationservice.entity.Notification;
+import edu.hm.peslalz.thesis.notificationservice.entity.UserMessage;
+import edu.hm.peslalz.thesis.notificationservice.service.NotificationService;
+import edu.hm.peslalz.thesis.notificationservice.service.PostReceiverService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class NotificationserviceApplicationTests {
 
+    @Autowired
+    private NotificationController notificationController;
+
+    @Autowired
+    private PostReceiverService postReceiverService;
+
+    @SpyBean
+    private NotificationService notificationService;
+
+    @MockBean
+    private UserClient userClient;
+
     @Test
-    void contextLoads() {
+    void receiveANewPost() throws JsonProcessingException {
+        List<UserMessage> userMessages = new ArrayList<>();
+        userMessages.add(new UserMessage(1,"contentLover",null,null,null));
+        userMessages.add(new UserMessage(2,"notificationsLover",null,null,null));
+        ResponseEntity<List<UserMessage>> responseEntity = ResponseEntity.ok().body(userMessages);
+        when(userClient.getUserFollowers(anyInt())).thenReturn(responseEntity);
+        postReceiverService.receive("""
+                {
+                  "id": 52,
+                  "userId": 3,
+                  "text": "New content new notification",
+                  "likes": 0,
+                  "comments": [],
+                  "categories": ["contentCreatorStrikesAgain", "takeThis Inbox"],
+                  "hasImage": true
+                }
+                """);
+        verify(userClient, times(1)).getUserFollowers(anyInt());
+        verify(notificationService, times(userMessages.size())).createNotification(any());
+        Assertions.assertThat(new File(NotificationService.OUTPUT_FOLDERNAME).listFiles()).hasSize(2);
     }
 
+    @Test
+    void controllerTests() {
+        Notification notification = new Notification();
+        notification.setId(3);
+        notification.setNotifiedUsersId(3);
+        notification.setPostId(3);
+        notification.setPostingUsersId(4);
+        notificationService.createNotification(notification);
+        Assertions.assertThat(notificationController.getUsersNotifications(3)).contains(notification);
+        Assertions.assertThat(notificationController.getNotificationCount(3, null)).isEqualTo(1);
+        Assertions.assertThat(notificationController.getNotificationCount(3, false)).isEqualTo(1);
+        notificationController.setReadStatus(3, true);
+        Assertions.assertThat(notificationController.getNotificationCount(3, true)).isEqualTo(1);
+    }
+
+    @AfterAll
+    static void afterAll() throws IOException {
+        File inbox = new File(NotificationService.OUTPUT_FOLDERNAME);
+        for (File file : inbox.listFiles()) {
+            FileUtils.deleteDirectory(file);
+        }
+        FileUtils.deleteDirectory(inbox);
+        if(inbox.getParentFile().listFiles().length == 0) {
+            FileUtils.deleteDirectory(inbox.getParentFile());
+        }
+
+    }
 }
