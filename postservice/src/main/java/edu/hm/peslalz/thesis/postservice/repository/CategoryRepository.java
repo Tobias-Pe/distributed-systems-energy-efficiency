f25cf6022c8415f3b4a7efd93c739193f5ffe33b
Tobias Peslalz <peslalz.tobias@gmail.com>
2024-05-25 17:12:39 +0200

    :bug: (posts) fix db-sided pagination by fetching id's and then joining them

--- postservice/src/main/java/edu/hm/peslalz/thesis/postservice/repository/CategoryRepository.java
+++ postservice/src/main/java/edu/hm/peslalz/thesis/postservice/repository/CategoryRepository.java
@@ -5,13 +5,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, String> {
+
     @Override
-    @EntityGraph(attributePaths = "posts")
-    Page<Category> findAll(Pageable pageable);
+    @EntityGraph(attributePaths = {"posts", "posts.comments"})
+    List<Category> findAllById(Iterable<String> strings);
+
+    @Query("select c.name from Category c")
+    Page<String> findAllNames(Pageable pageable);
 
     @EntityGraph(attributePaths = {"posts"})
     Optional<Category> findById(String id);
