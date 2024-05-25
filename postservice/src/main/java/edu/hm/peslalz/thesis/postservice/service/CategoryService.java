c1a054e8d88659bccafcec6854555b7071097980
Tobias Peslalz <peslalz.tobias@gmail.com>
2024-05-25 17:12:39 +0200

    :bug: (posts) fix db-sided pagination by fetching id's and then joining them

--- postservice/src/main/java/edu/hm/peslalz/thesis/postservice/service/CategoryService.java
+++ postservice/src/main/java/edu/hm/peslalz/thesis/postservice/service/CategoryService.java
@@ -3,9 +3,12 @@ package edu.hm.peslalz.thesis.postservice.service;
 import edu.hm.peslalz.thesis.postservice.entity.Category;
 import edu.hm.peslalz.thesis.postservice.repository.CategoryRepository;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
@@ -16,6 +19,9 @@ public class CategoryService {
     }
 
     public Page<Category> getCategories(@RequestParam(defaultValue = "0") int page) {
-        return categoryRepository.findAll(PageRequest.of(page, 50));
+        // don't page in memory --> page fetch only id's and then load the joined data
+        Page<String> allNames = categoryRepository.findAllNames(PageRequest.of(page, 50));
+        List<Category> joinedData = categoryRepository.findAllById(allNames.toSet());
+        return new PageImpl<>(joinedData);
     }
 }
