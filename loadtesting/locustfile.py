d910c3a049f4f6433f21c2a38b285dd16a1b7853
Tobias Peslalz <peslalz.tobias@gmail.com>
2024-05-25 17:53:41 +0200

    :wrench: (loadtesting) adjust probabilities for taskset execution

--- loadtesting/locustfile.py
+++ loadtesting/locustfile.py
@@ -7,4 +7,4 @@ from common.userservice import UserActions
 
 class GatewayUser(HttpUser):
     wait_time = between(0.5, 5)
-    tasks = [UserActions, PostActions, NotificationActions]
+    tasks = {UserActions: 3, PostActions: 3, NotificationActions: 1}
