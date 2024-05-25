8eaef7a80da5da51d0f8c6389d2ddcfd7538aa83
Tobias Peslalz <peslalz.tobias@gmail.com>
2024-05-25 17:51:12 +0200

    :sparkles: (loadtesting) implement notificationservice's endpoints

---
+++ loadtesting/common/notificationservice.py
@@ -0,0 +1,37 @@
+import random
+
+from locust import TaskSet, task
+
+from common.util import if_no_user_exists_wait, users, fake
+
+
+class NotificationActions(TaskSet):
+
+    @task
+    def create_user(self):
+        if_no_user_exists_wait()
+        user_id = random.choice(list(users.keys()))
+        self.client.post(f"/notificationservice/notifications/count?userId={user_id}",
+                         name="/notificationservice/notifications/count")
+
+    @task
+    def get_notifications(self):
+        if_no_user_exists_wait()
+        user_id = random.choice(list(users.keys()))
+        with self.client.get(f"/notificationservice/notifications?userId={user_id}",
+                             name="/notificationservice/notifications", catch_response=True) as response:
+            if response.status_code >= 400:
+                response.failure(response.text)
+                return
+            response.success()
+
+            # next task would be to change the read status of one of these notifications
+            notifications = response.json()
+            if notifications is None or len(notifications) == 0:
+                return
+            notification_id = random.choice(notifications).get("id")
+            self.change_notification_read_status(notification_id)
+
+    def change_notification_read_status(self, notification_id):
+        self.client.post(f"/notificationservice/notifications/{notification_id}?isRead={fake.boolean()}",
+                         name="/notificationservice/notifications/{id}")
