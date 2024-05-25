ff125786fa8473b5c2057f2e115af33efd0c215c
Tobias Peslalz <peslalz.tobias@gmail.com>
2024-05-25 17:51:12 +0200

    :sparkles: (loadtesting) implement notificationservice's endpoints

--- loadtesting/locustfile.py
+++ loadtesting/locustfile.py
@@ -1,9 +1,10 @@
 from locust import HttpUser, between
 
+from common.notificationservice import NotificationActions
 from common.postservice import PostActions
 from common.userservice import UserActions
 
 
 class GatewayUser(HttpUser):
     wait_time = between(0.5, 5)
-    tasks = [UserActions, PostActions]
+    tasks = [UserActions, PostActions, NotificationActions]
