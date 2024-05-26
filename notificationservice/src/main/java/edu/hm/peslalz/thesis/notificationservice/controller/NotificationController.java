ddf106398907cf1884ba4d73f3d22e068562201c
Tobias Peslalz <peslalz.tobias@gmail.com>
2024-05-26 13:49:04 +0200

    :loud_sound: (endpoints) log each endpoint action with minimal info

--- notificationservice/src/main/java/edu/hm/peslalz/thesis/notificationservice/controller/NotificationController.java
+++ notificationservice/src/main/java/edu/hm/peslalz/thesis/notificationservice/controller/NotificationController.java
@@ -3,12 +3,14 @@ package edu.hm.peslalz.thesis.notificationservice.controller;
 import edu.hm.peslalz.thesis.notificationservice.entity.Notification;
 import edu.hm.peslalz.thesis.notificationservice.service.NotificationService;
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
 @RestController
 @RequestMapping("notifications")
+@Log4j2
 public class NotificationController {
     NotificationService notificationService;
 
@@ -19,6 +21,7 @@ public class NotificationController {
     @Operation(summary = "Set the read status of the notification")
     @PostMapping("/{id}")
     public Notification setReadStatus(@PathVariable(name = "id") int notificationId, @RequestParam("isRead") boolean isRead) {
+        log.info("Setting read status of notification {} to {}", notificationId, isRead);
         return notificationService.setReadStatus(notificationId, isRead);
     }
 
@@ -26,12 +29,14 @@ public class NotificationController {
     @PostMapping("/count")
     public Long getNotificationCount(@RequestParam(name = "userId") int userId,
                                      @RequestParam(name = "wasRead", required = false) Boolean wasRead) {
+        log.info("Count notifications of user {} with query option {}", userId, wasRead);
         return notificationService.countNotificationOfUser(userId, wasRead);
     }
 
     @Operation(summary = "Find all notifications an user has")
     @GetMapping
     public Set<Notification> getUsersNotifications(@RequestParam(name = "userId") int userId) {
+        log.info("Find all notifications of user {}", userId);
         return notificationService.getUsersNotifications(userId);
     }
 
