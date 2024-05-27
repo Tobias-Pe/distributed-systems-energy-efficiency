b45a188fdeb828481b8982e94f1cd85512f8d272
Tobias Peslalz <peslalz.tobias@gmail.com>
2024-05-27 12:40:13 +0200

    :sparkles: (statisticservice) add new service

---
+++ statisticservice/src/main/java/edu/hm/peslalz/thesis/statisticservice/StatisticserviceApplication.java
@@ -0,0 +1,13 @@
+package edu.hm.peslalz.thesis.statisticservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticserviceApplication.class, args);
+	}
+
+}
