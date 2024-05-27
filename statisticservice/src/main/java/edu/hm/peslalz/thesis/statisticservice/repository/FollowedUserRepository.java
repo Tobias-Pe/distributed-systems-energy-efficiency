2d4d1b33ea326aeed0800bcde60146ca65415014
Tobias Peslalz <peslalz.tobias@gmail.com>
2024-05-27 18:45:18 +0200

    :sparkles: (statistic) add enpoints for post and user trends

--- statisticservice/src/main/java/edu/hm/peslalz/thesis/statisticservice/repository/FollowedUserRepository.java
+++ statisticservice/src/main/java/edu/hm/peslalz/thesis/statisticservice/repository/FollowedUserRepository.java
@@ -1,12 +1,27 @@
 package edu.hm.peslalz.thesis.statisticservice.repository;
 
 import edu.hm.peslalz.thesis.statisticservice.entity.FollowedUser;
+import edu.hm.peslalz.thesis.statisticservice.entity.TrendInterface;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Date;
 import java.util.Optional;
 
 public interface FollowedUserRepository extends JpaRepository<FollowedUser, Integer> {
+    @Query("""
+            select fu.userId as identifier, count(interactions) as trendPoints
+            from FollowedUser fu
+            inner join fu.interactions interactions
+            where interactions.registeredTimestamp > :oldestAllowedDate
+            GROUP BY fu.userId
+            ORDER BY COUNT(interactions) DESC
+            """)
+    Page<TrendInterface> findByMostInteractionsAfter(Date oldestAllowedDate, Pageable pageable);
+
     @EntityGraph(attributePaths = "interactions")
     Optional<FollowedUser> findByUserId(int userId);
 }
