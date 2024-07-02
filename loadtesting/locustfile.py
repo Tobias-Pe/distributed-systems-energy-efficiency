6a33291800b73556fc7e55caaeb60186c5497d9f
Tobias Peslalz <peslalz.tobias@gmail.com>
2024-07-02 22:47:23 +0200

    :sparkles: (loadtesting) change durations and user stages

--- loadtesting/locustfile.py
+++ loadtesting/locustfile.py
@@ -1,5 +1,5 @@
 from locust import HttpUser, between, LoadTestShape
-
+import locust.stats
 from common.feedservice import FeedActions
 from common.notificationservice import NotificationActions
 from common.postservice import PostActions
@@ -10,20 +10,29 @@ class GatewayUser(HttpUser):
     wait_time = between(0.5, 5)
     tasks = {UserActions: 30, PostActions: 30, FeedActions: 15, NotificationActions: 10}
 
-duration = 10
 
 stages = [
-    {"duration": duration*1, "users": 100, "spawn_rate":50},
-    {"duration": duration*2, "users": 200, "spawn_rate":50},
-    {"duration": duration*3, "users": 400, "spawn_rate":50}
+    {"duration": 60, "users": 100, "spawn_rate": 5},
+    {"duration": 40, "users": 200, "spawn_rate": 20},
+    {"duration": 40, "users": 300, "spawn_rate": 20},
+    {"duration": 60, "users": 500, "spawn_rate": 20},
+    {"duration": 80, "users": 800, "spawn_rate": 20},
+    {"duration": 80, "users": 1300, "spawn_rate": 50},
+    {"duration": 100, "users": 2100, "spawn_rate": 50}
 ]
+
+cumulative_duration = 0
+for stage in stages:
+    cumulative_duration += stage["duration"]
+    stage["cumulative_duration"] = cumulative_duration
+
 
 class StagedLoadShape(LoadTestShape):
     def tick(self):
         run_time = self.get_run_time()
 
         for stage in stages:
-            if run_time < stage["duration"]:
+            if run_time < stage["cumulative_duration"]:
                 # user count, spawn rate
                 tick_data = (stage["users"], stage["spawn_rate"])
                 return tick_data
